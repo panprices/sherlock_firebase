feaac6a670b5d6897c73035c00efc6eb889c2573
Sonja Ericsson <ericsson.m.sonja@gmail.com>
2020-08-22 20:16:14 +0200

    Catch exception

--- main.py
+++ main.py
@@ -93,28 +93,32 @@ def product_search_publish_result(event, context, production=True):
 		overwritten. 
 	"""
 
-	payload = json.loads(base64.b64decode(event['data']))
-	# Fetch the service account key JSON file contents
-	cred = credentials.Certificate('firebase_service_account.json')
-	# Initialize the app with a service account, granting admin privileges
-	app = firebase_admin.initialize_app(cred, {
-		'databaseURL': 'https://panprices.firebaseio.com/'
-	})
-	# Open a connection to the database
-	ref = db.reference('productSearch')
-	# Choose the relevant search
-	current_entry_ref = ref.child(str(payload['searchQuery']))
-	# Get existing data in this specific search 
-	current_entry = current_entry_ref.get({"results"})[0]
-	result = {}
-	# If results exist add to existing results dict
-	if current_entry != None:
-		result = current_entry["results"]
-	# Overwrites if gtin id already exists
-	result[payload["gtin"]] = payload
-	# Update the specific search in Firebase RTD with the newly fetched offers
-	current_entry_ref.update({
-		"results": result
-	})
-	# Kill the connection, otherwise the next instance trying to connect will crash
-	firebase_admin.delete_app(app)
+	try:
+		payload = json.loads(base64.b64decode(event['data']))
+		# Fetch the service account key JSON file contents
+		cred = credentials.Certificate('firebase_service_account.json')
+		# Initialize the app with a service account, granting admin privileges
+		app = firebase_admin.initialize_app(cred, {
+			'databaseURL': 'https://panprices.firebaseio.com/'
+		})
+		# Open a connection to the database
+		ref = db.reference('productSearch')
+		# Choose the relevant search
+		current_entry_ref = ref.child(str(payload['searchQuery']))
+		# Get existing data in this specific search 
+		current_entry = current_entry_ref.get({"results"})[0]
+		result = {}
+		# If results exist add to existing results dict
+		if current_entry != None:
+			result = current_entry["results"]
+		# Overwrites if gtin id already exists
+		result[payload["gtin"]] = payload
+		# Update the specific search in Firebase RTD with the newly fetched offers
+		current_entry_ref.update({
+			"results": result
+		})
+		# Kill the connection, otherwise the next instance trying to connect will crash
+		firebase_admin.delete_app(app)
+	except Exception as e:
+		raise e
+
