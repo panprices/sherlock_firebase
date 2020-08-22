659e207e8ab569c80096c1ba9b5f6476efc87ff8
Sonja Ericsson <ericsson.m.sonja@gmail.com>
2020-08-22 17:11:10 +0200

    Add function to publish results to firebase

--- demo.py
+++ demo.py
@@ -5,6 +5,7 @@ import base64
 from main import offer_search_trigger
 from main import live_search_offer_enricher
 from main import product_search_trigger
+from main import product_search_publish_result
 
 def demo_offer_search_trigger() :
 	# Mock a message
@@ -75,6 +76,32 @@ def demo_live_search_offer_enricher() :
 	result = live_search_offer_enricher(data, context, production=False)
 	print(result)
 
+def demo_product_search_publish_result() :
+	# Mock a message
+	message = {
+		"source":"google_shopping",
+		"product_name":"Amazon Fire HD 10 X-Shape combo case - Black ",
+		"gtin":"057145864027671",
+		"price":"37900",
+		"currency":"SEK",
+		"img_encoded":"",
+		"searchQuery":"Amazoooon Fire",
+		"img_url":"https://storage.googleapis.com/panprices/products/eb157654-0d76-3f61-861e-ad6b633f7df4.jpg",
+		"country":"NULL",
+		"product_url":"https://www.google.com/shopping/product/11209535067718676709",
+		"product_description":"For Amazon Fire HD 10 (2018) / (2017) [X-Shape] PC"
+   }
+	data = {
+		'data': base64.b64encode(json.dumps(message).encode())
+	}
+	# Define a mocked context
+	context = {
+		'event_id': '-1'
+	}
+	# Execute the function
+	result = product_search_publish_result(data, context, production=False)
+	print(result)
+
 def demo_product_search_trigger() :
 	# Mock a message
 	message = {
@@ -120,6 +147,12 @@ if __name__ == '__main__' :
 		action='store_true',
 		help='Listen to Realtime Firebase Triggers on productSearch and trigger to PubSub.'
 	)
+	parser.add_argument(
+		'-pspr',
+		'--product_search_publish_result',
+		action='store_true',
+		help='Publish to Realtime Firebase on productSearch.'
+	)
 	# Parse the args
 	args = parser.parse_args()
 	# Decide on execution
@@ -129,3 +162,6 @@ if __name__ == '__main__' :
 		demo_live_search_offer_enricher()
 	elif args.product_search_trigger :
 		demo_product_search_trigger()
+	elif args.product_search_publish_result:
+		demo_product_search_publish_result()
+
--- main.py
+++ main.py
@@ -84,3 +84,37 @@ def product_search_trigger(event, context, production=True):
 			raise e
 		print(pub_results)
 		return pub_results
+
+def product_search_publish_result(event, context, production=True):
+	"""
+		Listens to changes on pubsub output topic from google shopping
+		and writes final results to firebase. If there are previous results
+		on the search the function appends. If a gtin appears again, it gets
+		overwritten. 
+	"""
+
+	payload = json.loads(base64.b64decode(event['data']))
+	# Fetch the service account key JSON file contents
+	cred = credentials.Certificate('firebase_service_account.json')
+	# Initialize the app with a service account, granting admin privileges
+	app = firebase_admin.initialize_app(cred, {
+		'databaseURL': 'https://panprices.firebaseio.com/'
+	})
+	# Open a connection to the database
+	ref = db.reference('productSearch')
+	# Choose the relevant search
+	current_entry_ref = ref.child(str(payload['searchQuery']))
+	# Get existing data in this specific search 
+	current_entry = current_entry_ref.get({"results"})[0]
+	result = {}
+	# If results exist add to existing results dict
+	if current_entry != None:
+		result = current_entry["results"]
+	# Overwrites if gtin id already exists
+	result[payload["gtin"]] = payload
+	# Update the specific search in Firebase RTD with the newly fetched offers
+	current_entry_ref.update({
+		"results": result
+	})
+	# Kill the connection, otherwise the next instance trying to connect will crash
+	firebase_admin.delete_app(app)
