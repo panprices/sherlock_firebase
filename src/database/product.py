83a21488b568bd2cd88d573c6f275a6796b0aa7d
toanphan <toanphan.cs@gmail.com>
2021-01-25 16:48:14 +0100

    Add store offers products to the list of products to update frequently

--- src/database/product.py
+++ src/database/product.py
@@ -27,8 +27,17 @@ def _get_popular_products(cutoff_time, time_range=10):
         SELECT
             A.product_token
         FROM products A
-        INNER JOIN campaign_products B
-        ON A.id::text = B.product_id
+            INNER JOIN campaign_products B
+                ON A.id::text = B.product_id
+        WHERE A.rand_min_in_day >= {0}
+        AND A.rand_min_in_day < {1}
+        -- Grab the products from store offers
+        UNION ALL
+        SELECT 
+            A.product_token
+        FROM products A
+            INNER JOIN panprices_offers B
+                ON A.id = B.product_id
         WHERE A.rand_min_in_day >= {0}
         AND A.rand_min_in_day < {1}
     """.format(cutoff_time - time_range, cutoff_time))
