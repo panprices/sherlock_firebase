017ea5b047f5512f3062d1a6501b2f67e384210d
Julius Celik <julius.cc@panprices.com>
2021-05-12 16:26:51 +0200

    fix: add missing helper (oops)

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,3 +1,12 @@
-def format_search_offer_msg(firebase_msg) :
-	print(firebase_msg)
-	return(firebase_msg)
+def format_search_offer_msg(firebase_msg):
+    print(firebase_msg)
+    return firebase_msg
+
+
+def get_user_country_from_fb_context(context):
+    """
+    Input looks like: 'projects/_/instances/panprices/refs/offers/SE'
+    Output looks like: 'SE'
+    """
+    user_country = context.resource.split("/")[-1]
+    return user_country
