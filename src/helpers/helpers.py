2882b9da71cd1c1e9e1627bae296eccc85e09503
Julius Celik <julius.cc@panprices.com>
2021-05-14 14:31:08 +0200

    fix: collect country and not gtin from context

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -5,8 +5,8 @@ def format_search_offer_msg(firebase_msg):
 
 def get_user_country_from_fb_context(context):
     """
-    Input looks like: 'projects/_/instances/panprices/refs/offers/SE'
+    Input looks like: 'projects/_/instances/panprices/refs/offers/SE/gAAAAABghpvku8pg8Ic1s47bvU_7O6HZ8bZgdQHN6tCCEoIYcjjuUvr87hJxettqkH3KWzmwS2lc0077uQ6nJkxvOMRB9RGlUw=='
     Output looks like: 'SE'
     """
-    user_country = context.resource.split("/")[-1]
+    user_country = context.resource.split("/")[-2]
     return user_country
