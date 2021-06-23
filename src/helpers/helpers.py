5ad949def9b6248ff6f80a404907e6f76565b139
Toan Phan <toanphan.cs@gmail.com>
2021-06-23 14:04:30 +0200

    fix: separate the offer sources from by countries

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def format_search_offer_msg(firebase_msg):
     print(firebase_msg)
     return firebase_msg
@@ -10,3 +13,19 @@ def get_user_country_from_fb_context(context):
     """
     user_country = context.resource.split("/")[-2]
     return user_country
+
+
+def get_offer_sources() -> List[str]:
+    """Get a list of offer sources that are working stably."""
+    # TODO: change this to `select * from offer_sources after fixing all scrapers`
+    offer_sources = []
+    offer_sources.extend([f"ebay_{country}" for country in ("DE", "FR", "IT", "UK")])
+    offer_sources.extend(["prisjakt_SE", "prisjakt_FI"])
+    offer_sources.extend(["pricerunner_DK", "pricerunner_SE", "pricerunner_SE"])
+    offer_sources.extend(
+        [
+            f"kelkoo_{country}"
+            for country in ("DE", "DK", "FI", "FR", "NL", "NO", "SE", "UK")
+        ]
+    )
+    return offer_sources
