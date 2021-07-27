6823516f2fe7c559b60f7531bd52255ae144d2d0
Julius Celik <julius.cc@panprices.com>
2021-07-27 14:54:36 +0200

    Revert "chore: try pgbouncer with ppst"
    This reverts commit a93e4e1ca1a02553027a797da6648a2604cd3c33.

--- src/database/database.py
+++ src/database/database.py
@@ -3,31 +3,25 @@ from psycopg2.pool import SimpleConnectionPool
 import psycopg2.extras
 
 pg_config = {
-    "user": "postgres",
-    "password": "3pP49KAMpd0HefyA",
-    "dbname": "prices_prod",
+	'user': 'postgres',
+	'password': '3pP49KAMpd0HefyA',
+	'dbname': 'prices_prod'
 }
 
-
 def connect(host):
-    pg_config["host"] = host
-    return SimpleConnectionPool(1, 1, **pg_config)
+	pg_config['host'] = host
+	return SimpleConnectionPool(1, 1, **pg_config)
 
-
-def connect_to_db(use_pgbouncer=False):
-    try:
-        # pg_pool = connect('/cloudsql/panprices:europe-west1:panprices-psql')
-        if use_pgbouncer:
-            pg_pool = connect("34.77.105.192")
-        else:
-            pg_pool = connect("/cloudsql/panprices:europe-west1:panprices-core")
-            
-    except OperationalError as e:
-        # If production settings fail, use local development ones
-        pg_pool = connect("localhost")
-    # Remember to close SQL resources declared while running this function.
-    # Keep any declared in global scope (e.g. pg_pool) for later reuse.
-    with pg_pool.getconn() as connection:
-        cur = connection.cursor()
-        cur_dict = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        return [cur, cur_dict, connection, pg_pool]
+def connect_to_db() :
+	try:
+		# pg_pool = connect('/cloudsql/panprices:europe-west1:panprices-psql')
+		pg_pool = connect('/cloudsql/panprices:europe-west1:panprices-core')
+	except OperationalError as e:
+		# If production settings fail, use local development ones
+		pg_pool = connect('localhost')
+	# Remember to close SQL resources declared while running this function.
+	# Keep any declared in global scope (e.g. pg_pool) for later reuse.
+	with pg_pool.getconn() as connection:
+		cur = connection.cursor()
+		cur_dict = connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+		return [cur, cur_dict, connection, pg_pool]
