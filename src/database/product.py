db81bb76fbc7982ddf9329c6323c5e30041059e3
Julius Celik <julius.cc@panprices.com>
2021-07-27 14:54:36 +0200

    chore: try pgbouncer with ppst

--- src/database/product.py
+++ src/database/product.py
@@ -1,20 +1,26 @@
 from datetime import datetime
 from src.database.database import connect_to_db
 
+
 def get_popular_products():
     cutoff_time = _get_current_minute()
     return _get_popular_products(cutoff_time)
+
 
 def _get_current_minute():
     current_time = datetime.now()
     return current_time.minute + current_time.hour * 60
 
+
 def _get_popular_products(cutoff_time, time_range=10):
-    print(f"Fetching popular product from {cutoff_time - time_range} to {cutoff_time} mins of day")
+    print(
+        f"Fetching popular product from {cutoff_time - time_range} to {cutoff_time} mins of day"
+    )
 
-    cur, cur_dict, connection, pg_pool = connect_to_db()
+    cur, cur_dict, connection, pg_pool = connect_to_db(True)
 
-    cur.execute("""
+    cur.execute(
+        """
         -- Grab all the popular products
 		SELECT
             product_token
@@ -43,7 +49,10 @@ def _get_popular_products(cutoff_time, time_range=10):
         WHERE A.rand_min_in_day >= {0}
         AND A.rand_min_in_day < {1}
         AND product_token IS NOT NULL
-    """.format(cutoff_time - time_range, cutoff_time))
+    """.format(
+            cutoff_time - time_range, cutoff_time
+        )
+    )
     rows = cur.fetchall()
 
     pg_pool.putconn(connection)
