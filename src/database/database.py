15b5ebc888881c6f497e6f483e98ac60af9d7e1f
Julius Celik <julius.cc@panprices.com>
2021-08-12 14:24:06 +0200

    fix: consistent whitespace

--- src/database/database.py
+++ src/database/database.py
@@ -11,18 +11,18 @@ pg_config = {
 
 def connect(host):
     pg_config["host"] = host
-	return SimpleConnectionPool(1, 1, **pg_config)
+    return SimpleConnectionPool(1, 1, **pg_config)
 
 
 def connect_to_db():
-	try:
-		pg_pool = connect('/cloudsql/panprices:europe-west1:panprices-core')
-	except OperationalError as e:
-		# If production settings fail, use local development ones
+    try:
+        pg_pool = connect("/cloudsql/panprices:europe-west1:panprices-core")
+    except OperationalError as e:
+        # If production settings fail, use local development ones
         pg_pool = connect("localhost")
-	# Remember to close SQL resources declared while running this function.
-	# Keep any declared in global scope (e.g. pg_pool) for later reuse.
-	with pg_pool.getconn() as connection:
-		cur = connection.cursor()
+    # Remember to close SQL resources declared while running this function.
+    # Keep any declared in global scope (e.g. pg_pool) for later reuse.
+    with pg_pool.getconn() as connection:
+        cur = connection.cursor()
         cur_dict = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-		return [cur, cur_dict, connection, pg_pool]
+        return [cur, cur_dict, connection, pg_pool]
