df912eac618f8aa4f2f142bedd96f210d1f19e42
Toan Phan <toanphan.cs@gmail.com>
2021-10-11 10:51:14 +0200

    refactor: clean up demo.py

--- demo.py
+++ demo.py
@@ -15,12 +15,13 @@ from main import create_product_search_firebase
 
 def demo_offer_search_trigger():
     # Mock a message
+    product_token = "gAAAAABf3D0m2el6n0TB6D5fA-cNWVRCz_HEffoBPkQlb5oP2EU2_7AbWUCwj2145CVkW0C9No1DfWkRuLK5K8PLLu23J8UnFw=="
     message = {
         "data": None,
         "delta": {
             "created_at": 1623244478117,
             "offerFetchComplete": False,
-            "product_token": "gAAAAABf3D0m2el6n0TB6D5fA-cNWVRCz_HEffoBPkQlb5oP2EU2_7AbWUCwj2145CVkW0C9No1DfWkRuLK5K8PLLu23J8UnFw==",
+            "product_token": product_token,
             "triggered_from_client": True,
         },
     }
@@ -28,7 +29,9 @@ def demo_offer_search_trigger():
     class Context:
         def __init__(self) -> None:
             self.event_id = "-1"
-            self.resource = "projects/_/instances/panprices/refs/offers/SE/gAAAAABf3D0m2el6n0TB6D5fA-cNWVRCz_HEffoBPkQlb5oP2EU2_7AbWUCwj2145CVkW0C9No1DfWkRuLK5K8PLLu23J8UnFw=="
+            self.resource = (
+                f"projects/_/instances/panprices/refs/offers/SE/{product_token}"
+            )
 
     context = Context()
     # Execute the function
@@ -39,7 +42,6 @@ def demo_offer_search_trigger():
         context,
         production=True,
     )
-    print(result)
 
 
 def demo_live_search_offer_enricher():
