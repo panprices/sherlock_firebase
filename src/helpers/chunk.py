ebb68a6eb619c2d987ac4ce38fe6781c9ebe5e40
Julius Celik <julius.cc@panprices.com>
2021-10-27 16:00:09 +0200

    chore: add `chunks()` function

---
+++ src/helpers/chunk.py
@@ -0,0 +1,21 @@
+from typing import Any, List
+from itertools import islice
+
+
+def chunks(lst: List[Any], size: int) -> List[list]:
+    """
+    Split a list into size-sized chunks.
+
+    Example:
+
+    ```python
+    chunks([1, 2, 3, 4, 5], 2) -> [[1, 2], [3, 4], [5]]
+    ```
+    """
+
+    # This will get the start points for all chunks.
+    # Ex if you have a 100 long list and size is 24:
+    # range(0, 100, 24) -> [0, 24, 48, 72, 96]
+    indexes = range(0, len(lst), size)
+
+    return [lst[start : start + size] for start in indexes]
